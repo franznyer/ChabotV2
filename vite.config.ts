a43878d3fc521adba492884c0da24d01180c39c6
franznyer <testu.francois@gmail.com>
2024-07-15 17:01:16 +0200

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,10 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/.netlify/functions': {
-        target: 'http://localhost:8888',
+      '/api': {
+        target: 'http://localhost:5000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/\.netlify\/functions/, '')
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
