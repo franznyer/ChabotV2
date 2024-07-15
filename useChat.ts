41b216b3fe19ede953bedcc652a5e31f05fe3f56
franznyer <testu.francois@gmail.com>
2024-07-15 17:03:20 +0200

    useChat.ts

--- useChat.ts
+++ useChat.ts
@@ -7,7 +7,7 @@ export const useChat = () => {
   const sendMessage = async (message: string) => {
     setMessages([...messages, { user: 'me', text: message }]);
     try {
-      const response = await axios.post('/.netlify/functions/chat', { query: message });
+      const response = await axios.post('/api/chat', { query: message });
       setMessages([...messages, { user: 'me', text: message }, { user: 'bot', text: response.data.response }]);
     } catch (error) {
       console.error('Error sending message', error);
