3d837cc33872030d0015e697e57c838c522c3e05
franznyer <testu.francois@gmail.com>
2024-07-15 16:38:57 +0200

    App.tsx

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Welcome } from './components/Welcome';
+import './styles.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Welcome />
+    </div>
+  );
+};
+
+export default App;
